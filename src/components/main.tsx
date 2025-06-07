61e46b747bfc22ec0b8d2a038bdc334ad56f9301
Sofian <sofian.developpeur@gmail.com>
2025-06-08 01:07:48 +0200

    add separators to default text

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,11 +9,15 @@ import { toast } from "react-toastify";
 
 const testText = `# Markdown syntax guide
 
+---
+
 ## Headers
 
 # This is a Heading h1
 ## This is a Heading h2
 ###### This is a Heading h6
+
+---
 
 ## Emphasis
 
@@ -24,6 +28,8 @@ _This will also be italic_
 __This will also be bold__
 
 _You **can** combine them_
+
+---
 
 ## Lists
 
@@ -44,19 +50,27 @@ _You **can** combine them_
     1. Item 3a
     2. Item 3b
 
+---
+
 ## Images
 
 ![This is an alt text.](/image.png "This is a sample image.")
 
+---
+
 ## Links
 
 You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+
+---
 
 ## Blockquotes
 
 > Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
 >
 >> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+---
 
 ## Tables
 
@@ -66,12 +80,16 @@ You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
 | left bar      | right bar     |
 | left baz      | right baz     |
 
+---
+
 ## Blocks of code
 
 \`\`\`
 let message = 'Hello world';
 alert(message);
 \`\`\`
+
+Cool, isn't it?
 `
 
 export default function Main() {
